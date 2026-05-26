faca3a7c26ce8dc436e66dda124c34131272b459
1156775439-dotcom <1156775439@qq.com>
2026-05-26 09:16:01 +0800

    Set up Redux store with auth, product, and order reducers

---
+++ frontend/index.js
@@ -0,0 +1,14 @@
+import { configureStore } from '@reduxjs/toolkit';
+import authReducer from './authSlice';
+import productReducer from './productSlice';
+import orderReducer from './orderSlice';
+
+const store = configureStore({
+  reducer: {
+    auth: authReducer,
+    products: productReducer,
+    orders: orderReducer,
+  },
+});
+
+export default store;
